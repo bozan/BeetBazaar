897fc267cc95543327662ca7b322f3f4100423c7
irfanozer <irfanburak2000@hotmail.com>
2022-12-14 14:47:18 +0300

    signup api
    added signup api

--- beet_bazaar_app/server/index.js
+++ beet_bazaar_app/server/index.js
@@ -1,5 +1,6 @@
 //IMPORTS FROM PACKAGES
 const express = require('express');
+const mongoose = require('mongoose');
 
 //IMPORTS FROM OTHER FILES
 const authRouter = require('./routes/auth');
@@ -8,9 +9,22 @@ const authRouter = require('./routes/auth');
 //INIT
 const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://irfan:6uqIwfaWHBS5Q973@cluster0.vkerbhn.mongodb.net/?retryWrites=true&w=majority"; 
+
+//middleware
+//CLITEN -> middleware -> SERVER -> CLIENT
+app.use(express.json());
+app.use(authRouter);
+
+//connections
+mongoose.connect(DB).then(() => {
+    console.log('Connection successful');
+}).catch(e=>{
+    console.log(e);
+});
 
 
 //buraya porttan sonra virgul koyup ip yazmazsak localhosta baglaniyo
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`connected at port ${PORT}`);
 });
--- beet_bazaar_app/server/routes/auth.js
+++ beet_bazaar_app/server/routes/auth.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const User = require("../models/user");
+const bcryptjs = require('bcryptjs');
+
 
 const authRouter = express.Router();
 
-authRouter.get('/user', (req, res) => {
+/*authRouter.get('/user', (req, res) => {
     res.json({msg: "message"});
+});*/
+
+
+//SIGN UP
+authRouter.post("/api/signup", async (req, res) => {
+    try{
+        const  {name, email, password}  = req.body;
+
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({msg: "User with same email already exists!"});
+        }
+        const hashedPassword = await bcryptjs.hash(password, 8);
+
+        let user = new User({
+            email,
+            password: hashedPassword,
+            name,
+        })
+        user = await user.save();
+        res.json(user);
+        //get the data from client
+        // post that data in database
+        // return that data to the user
+    } catch(e){
+        res.status(500).json({error: e.message});
+    }
+    
 });
+
 
 module.exports = authRouter;
